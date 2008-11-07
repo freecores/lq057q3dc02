8679f2498a6cf1c5ed731fe88763c2340084a123
jwdonal <jwdonal@7fd88556-7d85-4c55-a196-c87b895015ab>
2008-11-07 01:33:15 +0000

    fixed syntax

--- implement/impact_go.bat
+++ implement/impact_go.bat
@@ -1,14 +1,14 @@
 @echo off
 REM --------------------------------------------------------------------------
 REM Modified: Jonathon W. Donaldson
-REM Rev-Mod:  $Id: impact_go.bat,v 1.1 2008-11-07 00:52:52 jwdonal Exp $
+REM Rev-Mod:  $Id: impact_go.bat,v 1.2 2008-11-07 01:33:15 jwdonal Exp $
 REM --------------------------------------------------------------------------
 
 REM --------------------------------------------------------------------------
 REM Script to target the FPGA with the resulting design bit file
 REM --------------------------------------------------------------------------
 
-echo 'SCRIPT: Which design would you like to use?'
+ECHO SCRIPT: Which design would you like to use?
 :GETDIR
 ECHO 0 - lq057q3dc02
 ECHO 1 - Do not use
@@ -22,20 +22,20 @@ ECHO .
 goto GETDIR
 
 :FAKEDLF
-echo 'SCRIPT: Running iMPACT...'
+ECHO SCRIPT: Running iMPACT...
 impact -batch impact_opt.cmd
 goto DONE
 
 :REALDLF
-echo 'SCRIPT: Running iMPACT...'
+ECHO SCRIPT: Running iMPACT...
 impact -batch impact_opt_dlf.cmd
 goto DONE
 
 :ALL
-echo 'SCRIPT: Running iMPACT...'
+ECHO SCRIPT: Running iMPACT...
 impact -batch impact_opt_all.cmd
 goto DONE
 
 :DONE
 del _impactbatch.log
-echo 'SCRIPT: Done!'
+ECHO SCRIPT: Done!
